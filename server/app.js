15e51f565383007e46f8ee1d4fc4ea66f807a605
Zhar.M <mehdizhar344@gmail.com>
2021-02-23 20:15:35 +0100

    Run Express & Angular Concurrently
    steps :
    cd client
    npm install concurrently -g
    adding proxy ( file proxy.config.json)
    thent npm start

--- server/app.js
+++ server/app.js
@@ -10,30 +10,35 @@ const cors = require('cors');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 var corsOptions = {
   origin: 'http://localhost:4200',
-  optionsSuccessStatus: 200 // For legacy browser support
-}
+  optionsSuccessStatus: 200, // For legacy browser support
+};
 //adding primary session
 app.use(cors(corsOptions));
-app.use(session({
-  secret:'keyboard cat',
-  resave:true,
-  saveUnitialized:true,
-  cookie:{secure: true}
-}))
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUnitialized: true,
+    cookie: { secure: true },
+  })
+);
 //adding public folders
-mongoose.connect(config.database,{useNewUrlParser:true , useUnifiedTopology: true});
-mongoose.connection.once('open', function(){
-  console.log("Database working fine");
-})
+mongoose.connect(config.database, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', function () {
+  console.log('Database working fine');
+});
 require('./routes/auth.routes')(app);
 require('./routes/request.routes')(app);
 require('./routes/response.routes')(app);
 require('./routes/claim.routes')(app);
 require('./routes/qrcode.routes')(app);
 var port = 3000;
-app.listen(port, function(){
-  console.info("Server Started on");
-})
+app.listen(port, function () {
+  console.info('Server Started on');
+});
